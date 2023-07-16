b0dedcadd1df3f1417e9f1c4fc4c96b4d35cf171
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-15 22:22:30 -0300

    Agregar estructura inicial de clase GoogleSteps

---
+++ src/test/java/steps/GoogleSteps.java
@@ -0,0 +1,31 @@
+package steps;
+
+// Comodin
+import io.cucumber.java.en.*;
+
+//import io.cucumber.java.en.And;
+//import io.cucumber.java.en.Given;
+//import io.cucumber.java.en.Then;
+//import io.cucumber.java.en.When;
+
+public class GoogleSteps {
+
+	// "^ TextoQueQuieroEvaluar $"
+	@Given("^I am on the Google search page$")
+	public void navigateToGoogle(){
+
+	}
+	@When("^ I enter a search criteria $")
+	public void enterSearchCriteria(){
+
+	}
+	@And("^click on the search button$")
+	public void clickSearchButton(){
+
+	}
+	@Then("^the results match the criteria$")
+	public void validateResults(){
+
+	}
+
+}
