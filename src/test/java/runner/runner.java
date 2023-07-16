543d4c6c8642aa741cf13c1468faf17f443c3ff3
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-16 20:02:18 -0300

    Agregar ajustes de dependencias y funcionalidad

---
+++ src/test/java/runner/runner.java
@@ -0,0 +1,21 @@
+// Esto es un archivo de configuracion en Cucumber y no una clase en si misma
+package runner;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+		features = "src/test/resources/features/",
+		glue = "steps"
+
+)
+
+/* *
+	Esta clase va a unir todas las funcionalidades, y los features
+ * */
+public class runner {
+
+}
+
+//System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
