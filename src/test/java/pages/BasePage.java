1fdbf8a9bbfdd30743a3b9197b53dc534ea8326f
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-07-16 21:08:56 -0300

    Ajustar nueva configuracion de clase Duration en las instancias driver y wait

--- src/test/java/pages/BasePage.java
+++ src/test/java/pages/BasePage.java
@@ -5,7 +5,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.time.Duration;
+
 public class BasePage {
+	// El WebDriver es la instancia del navegador que utilizamos para usar Selenium a nuestro gusto
 	// Creamos un objeto estatico para el driver
 	protected static WebDriver driver;
 	//Creamos un objeto estatico para el wait
@@ -15,14 +18,16 @@ public class BasePage {
 	// Y agregamos una espera de 10 segundos
 	static {
 		ChromeOptions chromeOptions = new ChromeOptions();
+		// Instancia del navegador
 		driver = new ChromeDriver(chromeOptions);
-		wait = new WebDriverWait(driver, 10);
+		// La instancia de la espera, en caso de que no conecte agregar una excepcion
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
 	// Constructor
 	public BasePage(WebDriver driver){
 		BasePage.driver = driver;
-		wait = new WebDriverWait(driver, 10);
+		wait = new WebDriverWait(driver, Duration.ofSeconds(10));
 	}
 
 	public static void navigateTo(String url){
