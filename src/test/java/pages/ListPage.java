82ee55518d04c93992442b11089f96ab642fd8fd
Cristian Emanuel Campos Fuentes <cristianemanuelcamposfuentes@hotmail.com>
2023-09-02 21:11:49 -0300

    Comenzando ListPage para probar metodos de List

--- src/test/java/pages/ListPage.java
+++ src/test/java/pages/ListPage.java
@@ -1,4 +1,20 @@
 package pages;
 
-public class ListPage {
+
+public class ListPage extends BasePage {
+    private String searchField = "//body/form[1]/input[1]";
+    private String searchResults = "name";
+
+    public ListPage() {
+        super(driver);
+    }
+
+    public void navigateToListPage(){
+        navigateTo("https://twitter.github.io/typeahead.js/examples/");
+    }
+
+    public void enterSearchCriteria() throws InterruptedException {
+
+    }
+
 }
